d53199fa50fc878976dcea501c899bdfdc63d51f
LRosdobutko <lrosdobutko@academic.rrc.ca>
2023-07-24 14:43:22 -0500

    Created CatPicture object.

---
+++ CatAPI/CatPicture.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace KL.CatAPI
+{
+    public class CatPicture
+    {
+        //string id { get; set; }
+        //string url { get; set; }
+        //int width { get; set; }
+        //int height { get; set; }
+        //bool breeds { get; set; }
+
+        [JsonPropertyName("id")]
+        public string? Id { get; set; }
+
+        [JsonPropertyName ("url")]
+        public string? Url { get; set; }
+
+        [JsonPropertyName("height")]
+        public int Height { get; set; }
+
+        [JsonPropertyName("width")]
+        public int Width { get; set; } 
+
+    }
+}
